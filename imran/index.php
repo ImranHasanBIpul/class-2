01b82a95b9894bf539cb7d50ca9df220a83f25d6
designerimranali <designerimranali@gmail.com>
2022-03-24 21:20:34 +0600

    PHP class - 2

--- imran/index.php
+++ imran/index.php
@@ -1,9 +1,13 @@
 <?Php
-$Name = "Imran Hasan";
-$name2 = "Bipul";
-$profession = "Student";
-$profession1 = "Web Designer";
-$age = "25";
+$Name = "Imran Hasan"; //Datatype string
+$name2 = "Bipul"; //Datatype string
+$profession = "Student"; //Datatype string
+$profession1 = "Web Designer"; //Datatype string
+$age = 25; //Datatype integer
+$a = 40;  //Datatype integer
+$b = 50;  //Datatype integer
+
+
 echo "Name:" ." ". $Name;
 echo "<br/>";
 echo "Nick Name:" ." ". $name2;
@@ -17,20 +21,12 @@ echo "<br/>";
 define('address', 'Address: Mymensing, Dhaka');
 echo address;
 echo "<br/>";
+echo $a + $b; 
+echo "<br/>";
 $name = "imran"; //string
 $number = 20; // intiger
 $amount = 15.50; // floatval
 $switch = "true" // boolean
-
-
-
-
-
-
-
-
-
-
 
 
 
