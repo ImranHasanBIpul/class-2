bb072cf9e7e84c53214d16737878b15fd7e28acb
designerimranali <designerimranali@gmail.com>
2022-03-25 15:19:38 +0600

    PHP class - 2

--- imran/index.php
+++ imran/index.php
@@ -1,4 +1,40 @@
 <?Php
+   /* Case Study:
+	I have three old hp monitors, not new. I want to sell it on my own website. 
+	That’s why I should create a product upload form on my website, then upload 
+	my product with specification and available quantity. My monitors have a 15.6 
+	inch display. My monitor’s VGA cable status, yes, I will sell it for 8000 tk.*/
+
+$hi = "Hi There";
+echo $hi;
+echo "<br/>";
+$hello = "Hello";
+echo $hello;
+echo "<br/>";
+$product = "I have three old hp monitors.";
+echo $product;
+echo "<br/>";
+$website = "I want to sell it on my own website.";
+echo $website;
+echo "<br/>";
+$upload = "That’s why I should create a product upload form on my website";
+echo $upload;
+echo "<br/>";
+$quantity = "my product with specification and available quantity.";
+echo $quantity;
+echo "<br/>";
+$display = 15.6; // float
+echo $display;
+echo "<br/>";
+$price = 8000; // integer
+echo $price;
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+echo "<br/>";
+
+
+
 $Name = "Imran Hasan"; //Datatype string
 $name2 = "Bipul"; //Datatype string
 $profession = "Student"; //Datatype string
@@ -27,8 +63,6 @@ $name = "imran"; //string
 $number = 20; // intiger
 $amount = 15.50; // floatval
 $switch = "true" // boolean
-
-
 
 
 
